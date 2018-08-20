5ced79d4c06d37540f14d49524274908807deb7b
WrathZA <miller.brettm@gmail.com>
2018-08-20 07:52:21 +0200

    fuck

--- presentation/index.js
+++ presentation/index.js
@@ -1,3 +1,5 @@
+// https://github.com/basarat/typescript-book/blob/master/docs/types/discriminated-unions.md
+
 // Import React
 import React from "react";
 
@@ -14,7 +16,8 @@ import {
   Text,
   Notes,
   Image,
-  Appear
+  Appear,
+  CodePane
 } from "spectacle";
 
 import preloader from "spectacle/lib/utils/preloader";
@@ -31,7 +34,8 @@ const images = {
   js: require("../assets/js.png"),
   ts: require("../assets/ts.png"),
   anders: require("../assets/anders.png"),
-  cptmsug: require("../assets/ctmsug.jpg")
+  cptmsug: require("../assets/ctmsug.jpg"),
+  tsgraph: require("../assets/tsgraph.png")
 };
 
 preloader(images);
@@ -68,9 +72,7 @@ export default class Presentation extends React.Component {
           <Image margin="50px auto" src={images.son.replace("/", "")} margin="0px auto 50px" height="700px" />
         </Slide>
 
-        <Slide transition={["zoom"]} bgColor="primary">
-          <Image margin="50px auto" src={images.ts.replace("/", "")} margin="0px auto 50px" height="700px" />
-        </Slide>
+
 
         <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
           <Heading margin={"0px auto 20px"} size={1} fill caps lineHeight={1} textColor="tertiary">
@@ -98,7 +100,7 @@ export default class Presentation extends React.Component {
         </Slide>
 
         <Slide transition={["zoom"]} bgColor="primary">
-          <Image margin="50px auto" src={images.js.replace("/", "")} margin="0px auto 50px" height="700px" />
+          <Image src={images.js.replace("/", "")} margin="0px auto 50px" height="700px" />
         </Slide>
 
         <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
@@ -108,11 +110,96 @@ export default class Presentation extends React.Component {
           <BlockQuote>
             JS is a high-level, interpreted programming language. It's dynamic, weakly typed, prototype-based and multi-paradigm
           </BlockQuote>
-          <Appear>
-            <BlockQuote>
-              JavaScript is an event-driven and supports BOTH functional AND imperative (including object-oriented and prototype-based) programming styles
+          <BlockQuote>
+            JavaScript is an event-driven and supports BOTH functional AND imperative (including object-oriented and prototype-based) programming styles
           </BlockQuote>
+          <BlockQuote>
+            Single threaded event loop architecture with background workers
+          </BlockQuote>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <Heading size={1} textColor="tertiary" caps>JavaScript Runtime</Heading>
+          <Text margin="30px auto" textSize="66" textColor="primary" bold>
+            Single threaded event driven (loop) architecture with background workers (concurrent)
+          </Text>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <Heading size={2} textColor="primary" caps>JavaScript Types</Heading>
+          <Text textSize="66" textColor="primary" bold>
+            <dl>
+              <dt>Boolean</dt>
+              <dt>Number</dt>
+              <dt>String</dt>
+              <dt>Null</dt>
+              <dt>Undefined</dt>
+              <dt>---</dt>
+              <dt>Object {"{"} {"}"}</dt>
+            </dl>
+          </Text>
+          <Appear>
+            <Text textSize="80" margin="50px 0 0" textColor="tertiary" bold>
+              Arrays are objects
+            </Text>
           </Appear>
+          <Appear>
+            <Text textSize="100" margin="50px 0 0" textColor="tertiary" bold>
+              *FUNCTIONS() are OBJECTS*
+            </Text>
+          </Appear>
+        </Slide>
+
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Image margin="50px auto" src={images.ts.replace("/", "")} margin="0px auto 50px" height="700px" />
+        </Slide>
+
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={2} fill lineHeight={1} textColor="secondary">
+            JS === TS, but TS !== JS
+          </Heading>
+          <Image src={images.tsgraph.replace("/", "")} margin="50px auto" height="700px" />
+        </Slide>
+
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={1} fill lineHeight={1} textColor="secondary">
+            What is TS?
+          </Heading>
+          <Text textSize="70" margin="50px 0 0" textColor="tertiary" bold fill caps>
+            It's development tooling.
+          </Text>
+          <Appear>
+          <Text textSize="55" margin="50px 0 0" textColor="secondary" bold fill caps>
+            Transpiler
+          </Text>
+          </Appear>
+          <Appear>
+          <Text textSize="55" margin="50px 0 0" textColor="secondary" bold fill caps>
+            Optional Type checking at compile time.
+          </Text>
+          </Appear>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <Heading size={2} textColor="primary" caps>TRANSPILER EXAMPLE</Heading>
+        </Slide>
+
+        <Slide>
+          <Heading size={2} textColor="secondary" caps>ASYNC TIMEOUT</Heading>
+          <CodePane textSize={40}
+            lang="js"
+            source={require("raw-loader!../assets/asynctimeout.code")}
+            margin="20px auto"
+          />
+        </Slide>
+
+         <Slide>
+          <Heading size={2} textColor="secondary" caps>TIMEOUT</Heading>
+          <CodePane textSize={40}
+            lang="js"
+            source={require("raw-loader!../assets/timeoutPromise.code")}
+            margin="20px auto"
+          />
         </Slide>
 
       </Deck>
